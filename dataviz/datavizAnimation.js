e07c402ffe4b512d869dddcce670707963ba08a5
Laureuh <laure.sterne@student.adatechschool.fr>
2021-11-15 16:48:36 +0100

    the start of dataviz

---
+++ dataviz/datavizAnimation.js
@@ -0,0 +1,13 @@
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    m = checkTime(m);
+    document.getElementById('txt').innerHTML =  h + ":" + m;
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
